e299b6b1d28334a2b0a8928f23ff0b50cbffe1d3
Denisshef <denisshef24@yandex.ru>
2022-03-11 14:15:59 +0700

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val buyPastMonth = true
+    var buyThisMonth = 15000.0
+
+    if ((buyThisMonth > 1000) && (buyThisMonth <= 10000)) {
+        buyThisMonth -= 100
+        println("==> скидка при покупке свыше 1000 р. -> 100 р.")
+    }
+
+    if (buyThisMonth > 10000) {
+        println("==> скидка 5% -> ${buyThisMonth * 0.05} р.")
+        buyThisMonth -= (buyThisMonth * 0.05)
+    }
+
+    if (buyPastMonth) {
+        println("==> скидка \"Меломан\" 1% -> ${buyThisMonth * 0.01} р.")
+        buyThisMonth -= (buyThisMonth * 0.01)
+    }
+
+    println("Итого к оплате с учетом всех скидок -> $buyThisMonth р.")
+}
